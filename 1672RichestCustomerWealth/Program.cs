3185b43364f55ef29974b5f886b2ad411a149d18
Duncan <96219296+LoneROKIE@users.noreply.github.com>
2023-09-13 13:15:03 -0600

    new solution

---
+++ 1672RichestCustomerWealth/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+    You are given an m x n integer grid accounts where accounts[i][j] is the amount of money 
+    the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+    A customer's wealth is the amount of money they have in all their bank accounts.
+    The richest customer is the customer that has the maximum wealth.
+ */
+
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        MaximumWealth(new int[][] {new int[] {1,2,3}, new int[] {3,2,1} });
+    }
+
+    public static void MaximumWealth(int[][] accounts)
+    {
+       int[] arreglo = new int[accounts.Length];
+        for (int i=0; i < accounts.Length; i++)
+        {
+           
+            for(int j=0; j < accounts[i].Length; j++)
+            {
+                Console.Write(accounts[i][j]+" ");
+                arreglo[i] += accounts[i][j];
+            }
+            Console.WriteLine(" ");
+        }
+        int aux = 0;
+        for (int i = 0; i < arreglo.Length; i++)
+        {
+            if(aux <= arreglo[i])
+            {
+                aux = arreglo[i];
+            }
+            Console.WriteLine(arreglo[i]);
+        }
+        Console.WriteLine("MAYOR MILLONARIO: "+aux);
+
+    }
+}
