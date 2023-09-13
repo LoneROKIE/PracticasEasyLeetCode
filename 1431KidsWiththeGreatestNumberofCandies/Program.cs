9942048f9cf860517d81c71c09a99550287cb086
Duncan <96219296+LoneROKIE@users.noreply.github.com>
2023-09-13 07:20:14 -0600

    new solution

---
+++ 1431KidsWiththeGreatestNumberofCandies/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Solution
+{
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+
+        // sacamos el valor mayor de candies
+        int mayor = 0;
+        for (int i = 0; i < candies.Length; i++)
+        {
+            if (mayor < candies[i])
+            {
+                mayor = candies[i];
+            }
+        }
+        // creamos un array de bool
+        bool[] resultado = new bool[candies.Length];
+        for (int i = 0; i < candies.Length; i++)
+        {
+            if (candies[i] + extraCandies >= mayor)
+            {
+                resultado[i] = true;
+            }
+            else
+            {
+                resultado[i] = false;
+            }
+        }
+
+        return resultado;
+    }
+}
