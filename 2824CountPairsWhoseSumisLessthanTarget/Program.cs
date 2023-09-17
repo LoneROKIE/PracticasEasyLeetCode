77b5ff300b1e2de0c03889bb36853429c1ab46dc
Duncan <96219296+LoneROKIE@users.noreply.github.com>
2023-09-17 10:09:07 -0600

    new solution problem 2824

---
+++ 2824CountPairsWhoseSumisLessthanTarget/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int CountPairs(IList<int> nums, int target)
+    {
+
+        int conteo = 0;
+
+        for (int i = 0; i < nums.Count; i++)
+        {
+            for (int j = 0; j < nums.Count; j++)
+            {
+                if (0 <= i && i < j && j < nums.Count && nums[i] + nums[j] < target)
+                {
+                    conteo++;
+                }
+            }
+        }
+        return conteo;
+    }
+}
