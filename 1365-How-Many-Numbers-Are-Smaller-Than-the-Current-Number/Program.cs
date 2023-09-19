eb275bdf71d2166e77a3675af075196287a2192e
Duncan <96219296+LoneROKIE@users.noreply.github.com>
2023-09-18 21:52:54 -0600

    no solved it

---
+++ 1365-How-Many-Numbers-Are-Smaller-Than-the-Current-Number/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it.
+That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+Return the answer in an array.
+ */
+
+public class Solution
+{
+
+    public static void Main(string[] args)
+    {
+
+    }
+
+    public static int[] SmallerNumbersThanCurrent(int[] nums)
+    {
+
+    }
+
+}
