7e45511a8af7f179937aa290bed6a98b970c60fc
Duncan <96219296+LoneROKIE@users.noreply.github.com>
2023-09-19 22:01:01 -0600

    nueva solucion
    solucion nueva, pero lamentablemente no es mia :( tuve que ver la solucion porque no pude resolver el problema

--- 1365-How-Many-Numbers-Are-Smaller-Than-the-Current-Number/Program.cs
+++ 1365-How-Many-Numbers-Are-Smaller-Than-the-Current-Number/Program.cs
@@ -6,15 +6,34 @@ Return the answer in an array.
 
 public class Solution
 {
-
-    public static void Main(string[] args)
+    public int[] SmallerNumbersThanCurrent(int[] nums)
     {
+        // nuevo arreglo respuesta
+        int[] respuesta = new int[nums.Length];
+        int longitud = 0;
 
+        foreach (int num in nums)
+        {
+            int smallers = CountSmallers(num, nums, longitud);
+            {
+                respuesta[longitud++] = smallers;
+            }
+        }
+        return respuesta;
     }
 
-    public static int[] SmallerNumbersThanCurrent(int[] nums)
+    public int CountSmallers(int num, int[] arr, int start)
     {
+        int count = 0;
 
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] < num)
+            {
+                count++;
+            }
+        }
+
+        return count;
     }
-
 }
