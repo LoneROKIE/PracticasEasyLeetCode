a65ac5f6a0782d4f32a292b47be6e63a35c18bad
Duncan <96219296+LoneROKIE@users.noreply.github.com>
2023-09-20 22:07:36 -0600

    new solution 771 Jewels and stones

---
+++ 771-Jewels-and-Stones/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+
+Input: jewels = "aA", stones = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: jewels = "z", stones = "ZZ"
+Output: 0 
+ */
+
+using System.Text.RegularExpressions;
+public class Solution
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(NumJewelsInStones("aA", "aAAbbbb"));
+    }
+
+    public static int NumJewelsInStones(string jewels, string stones)
+    {
+
+        // Construir una expresión regular que represente todas las joyas en "jewels"
+        string pattern = "[" + Regex.Escape(jewels) + "]";
+
+        int numeroOcurrencias = Regex.Matches(stones, pattern).Count();
+        return numeroOcurrencias;
+    }
+
+
+
+}
